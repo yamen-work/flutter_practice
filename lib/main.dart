5b17f1929a3dd566d7872913eae745022e31c6cd
Yamen AN <yamenabounabout@gmail.com>
2026-06-22 18:21:56 +0300

    edit: edit color to blue

--- lib/main.dart
+++ lib/main.dart
@@ -111,6 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
+        backgroundColor: Colors.blue,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
